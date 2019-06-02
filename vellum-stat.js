4527a931bf74c669e9e0190cb3ca539d0b55499e
R.G. Wood <ric@grislyeye.com>
2019-06-02 15:40:16 +0100

    Convert <vellum-stat> to LitElement
    As part of this issue:
    
    https://github.com/grislyeye/vellum-monster/issues/33

--- vellum-stat.js
+++ vellum-stat.js
@@ -1,113 +1,83 @@
-import { PolymerElement } from '../@polymer/polymer/polymer-element.js'
-import '../@polymer/polymer/lib/elements/dom-if.js'
-import { html } from '../@polymer/polymer/lib/utils/html-tag.js'
+import { LitElement, html, css } from 'lit-element'
 
-class Stat extends PolymerElement {
+class Stat extends LitElement {
 
-  static get template() {
-    return html`
-    <style>
-      .stat-name {
-        font-weight: bold;
-      }
+  static get styles() {
+    return css`
+    .stat-name {
+      font-weight: bold;
+    }
 
-      p {
-        margin-top: 1pt;
-        margin-bottom: 0;
-        margin-left: 0;
-        margin-right: 0;
-        padding-left: 1em;
-        text-indent: -1em;
-      }
+    p {
+      margin-top: 1pt;
+      margin-bottom: 0;
+      margin-left: 0;
+      margin-right: 0;
+      padding-left: 1em;
+      text-indent: -1em;
+    }
 
-      :host(.trait) p,
-      :host(.action) p {
-        margin-top: 0;
-        margin-bottom: 1em;
-        margin-left: 0;
-        margin-right: 0;
-        padding-left: 0;
-        text-indent: 0;
-      }
+    :host(.trait) p,
+    :host(.action) p {
+      margin-top: 0;
+      margin-bottom: 1em;
+      margin-left: 0;
+      margin-right: 0;
+      padding-left: 0;
+      text-indent: 0;
+    }
 
-      :host(.table-row) p {
-        padding-left: 0;
-        text-indent: 1em;
-      }
+    :host(.table-row) p {
+      padding-left: 0;
+      text-indent: 1em;
+    }
 
-      :host(.trait) .stat-name,
-      :host(.action) .stat-name {
-        font-style: italic;
-      }
-    </style>
-
-    <dom-if if="{{hasDescription}}">
-      <template>
-
-        <dom-if if="{{hasValues}}">
-          <template>
-            <p>
-              <span class="stat-name">{{name}}</span>
-              <span class="stat-description">{{displayText}}</span>
-            </p>
-          </template>
-        </dom-if>
-
-        <dom-if if="{{!hasValues}}">
-          <template>
-            <p>
-              <span class="stat-name">{{name}}</span>
-              <span id="description" class="stat-description"><slot></slot></span>
-            </p>
-          </template>
-        </dom-if>
-
-      </template>
-    </dom-if>`
+    :host(.trait) .stat-name,
+    :host(.action) .stat-name {
+      font-style: italic;
+    }`
   }
-
-  static get is() { return 'vellum-stat' }
 
   static get properties() {
     return {
       name: String,
       values: {
-        type: Object,
-        value: '_not-set'
-      },
-      hasDescription: {
-        type: Boolean,
-        computed: '_hasDescription(values)'
-      },
-      hasValues: {
         type: String,
-        computed: '_hasValues(values)'
-      },
-      displayText: {
-        type: String,
-        computed: '_displayText(values)'
+        hasChanged(newVal, oldVal) {
+          return newVal !== oldVal
+        }
       }
     }
   }
 
-  _hasDescription(values) {
-    const innerHTML = this.innerHTML.trim()
-    return (values !== '_not-set' || innerHTML !== '')
-  }
-
-  _hasValues(values) {
-    return Array.isArray(values) || values !== '_not-set'
-  }
-
-  _displayText(values) {
-    if (Array.isArray(values)) {
-      return values.join(', ')
-    } else if (values !== '_not-set') {
-      return values
-    } else {
-      return undefined
+  render() {
+    if (this.hasDescription()) {
+      return html`<p>
+        <span class="stat-name">${this.name}</span>
+        <span class="stat-description">${this.description()}</span>
+      </p>`
     }
   }
+
+  hasDescription() {
+    const innerDescription = this.innerHTML.trim()
+    return (this.values || innerDescription !== '')
+  }
+
+  description() {
+    if (Array.isArray(this.values)) {
+      return this.values.join(', ')
+    }
+
+    // TODO replace this try...catch block that could hide important error messages
+    try {
+      return JSON.parse(this.values).join(', ')
+    } catch (error) {
+      if (this.values) return this.values
+      else return html`<slot></slot>`
+    }
+  }
+
 }
 
-customElements.define(Stat.is, Stat)
+customElements.define('vellum-stat', Stat)
