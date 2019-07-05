6105d5533866de6ebd46090f367d7b059b604bd6
R.G. Wood <ric@grislyeye.com>
2019-07-05 12:42:54 +0100

    Convert <vellum-stat-block-divider> to LitElement
    As part of this issue:
    
    https://github.com/grislyeye/vellum-monster/issues/33

--- vellum-stat-block-divider.js
+++ vellum-stat-block-divider.js
@@ -1,20 +1,20 @@
-import { PolymerElement } from '../@polymer/polymer/polymer-element.js'
-import { html } from '../@polymer/polymer/lib/utils/html-tag.js'
+import { LitElement, html, css } from 'lit-element'
 
-class StatBlockDivider extends PolymerElement {
+class StatBlockDivider extends LitElement {
 
-  static get template() {
+  static get styles() {
+    css`
+    svg {
+      fill: var(--stat-block-divider-color, lightgrey);
+      /* Stroke is necessary for good antialiasing in Chrome. */
+      stroke: var(--stat-block-divider-color, lightgrey);
+      margin-top: 0.6em;
+      margin-bottom: 0.35em;
+    }`
+  }
+
+  render() {
     return html`
-    <style>
-      svg {
-        fill: var(--stat-block-divider-color, lightgrey);
-        /* Stroke is necessary for good antialiasing in Chrome. */
-        stroke: var(--stat-block-divider-color, lightgrey);
-        margin-top: 0.6em;
-        margin-bottom: 0.35em;
-      }
-    </style>
-
     <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>`
   }
 
