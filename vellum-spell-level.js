3b22b01ea650abff09b79bd605c9460c4055a7c4
R.G. Wood <ric@grislyeye.com>
2019-07-07 11:28:42 +0100

    Convert <vellum-spell-level> to LitElement
    As part of this issue:
    
    https://github.com/grislyeye/vellum-monster/issues/33

--- vellum-spell-level.js
+++ vellum-spell-level.js
@@ -1,59 +1,12 @@
-import { PolymerElement } from '../@polymer/polymer/polymer-element.js'
-import '../@polymer/polymer/lib/elements/dom-if.js'
-import { html } from '../@polymer/polymer/lib/utils/html-tag.js'
+import { LitElement, html, css } from 'lit-element'
 
-class SpellLevel extends PolymerElement {
+class SpellLevel extends LitElement {
 
-  static get template() {
-    return html`
-    <style>
-      .spells {
-        font-style: italic;
-      }
-    </style>
-
-    <span class="spell-level">
-      <span class="level">
-        <dom-if if="{{cantrips}}">
-          <template>
-            Cantrips
-          </template>
-        </dom-if>
-
-        <dom-if if="{{!cantrips}}">
-          <template>
-            {{level}} level
-          </template>
-        </dom-if>
-
-        <dom-if if="{{hasSlots}}">
-          <template>
-            ({{slots}}
-
-            <dom-if if="{{hasMultipleSlots}}">
-              <template>
-                slots):
-              </template>
-            </dom-if>
-
-            <dom-if if="{{!hasMultipleSlots}}">
-              <template>
-                slot):
-              </template>
-            </dom-if>
-
-          </template>
-        </dom-if>
-
-        <dom-if if="{{!hasSlots}}">
-          <template>
-            (at will):
-          </template>
-        </dom-if>
-      </span>
-
-      <span class="spells">{{spellsDisplay}}</span>
-    </span>`
+  static get styles() {
+    return css`
+    .spells {
+      font-style: italic;
+    }`
   }
 
   static get is() { return 'vellum-spell-level' }
@@ -61,47 +14,41 @@ class SpellLevel extends PolymerElement {
   static get properties() {
     return {
       level: String,
-      cantrips: {
-        type: Boolean,
-        computed: '_cantrips(level)'
-      },
-      slots: {
-        type: Number,
-        value: 0
-      },
-      hasSlots: {
-        type: Boolean,
-        computed: '_hasSlots(slots)'
-      },
-      hasMultipleSlots: {
-        type: Boolean,
-        computed: '_hasMultipleSlots(slots)'
-      },
-      spells: Object,
-      spellsDisplay: {
-        type: String,
-        computed: '_spellsDisplay(spells)'
-      }
+      spells: Array,
+      slots: Number
     }
   }
 
-  _cantrips(level) {
+  render() {
+    return html`
+    <span class="spell-level">
+      <span class="level">
+        ${this.hasCantrips(this.level) ? html`Cantrips` : html`${this.level} level`}
+        ${this.slots > 0 ? this.renderSlots() : html`(at will):`}
+      </span>
+
+      <span class="spells">${this.renderSpells()}</span>
+    </span>`
+  }
+
+  hasCantrips(level) {
     return level.toLowerCase() === 'cantrips'
   }
 
-  _hasSlots(slots) {
-    return slots > 0
+  renderSlots() {
+    return html`
+    (${this.slots} ${this.slots > 1 ? html`slots):` : html`slot):`}`
   }
 
-  _hasMultipleSlots(slots) {
-    return slots > 1
-  }
-
-  _spellsDisplay(spells) {
-    if (Array.isArray(spells)) {
-      return spells.join(', ')
+  renderSpells() {
+    if (Array.isArray(this.spells)) {
+      return this.spells.join(', ')
     } else {
-      return spells
+      try {
+        return JSON.parse(this.spells).join(', ')
+      } catch (error) {
+        return this.spells
+      }
     }
   }
 }
