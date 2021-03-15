374cdb8a77f586b4d5ed6e13491bdbeda5e02ccd
R.G. Wood <ric@grislyeye.com>
2021-03-15 15:48:46 +0000

    Convert <vellum-stat-block-divider> to Typescript

--- vellum-stat-block-divider.ts
+++ vellum-stat-block-divider.ts
@@ -1,6 +1,7 @@
-import { LitElement, html, css } from 'lit-element'
+import { LitElement, html, css, customElement, TemplateResult } from 'lit-element'
 
-class StatBlockDivider extends LitElement {
+@customElement('vellum-stat-block-divider')
+export class StatBlockDivider extends LitElement {
 
   static get styles() {
     return css`
@@ -13,13 +14,9 @@ class StatBlockDivider extends LitElement {
     }`
   }
 
-  render() {
+  render(): TemplateResult | void {
     return html`
     <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>`
   }
 
-  static get is() { return 'vellum-stat-block-divider' }
-
 }
-
-customElements.define(StatBlockDivider.is, StatBlockDivider)
