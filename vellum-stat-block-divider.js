d3e42cc11e05f601e02826090095d7a414944fd7
R.G. Wood <ric@grislyeye.com>
2022-01-20 15:43:06 +0000

    [fixup] fix partial reversion

--- vellum-stat-block-divider.js
+++ vellum-stat-block-divider.js
@@ -1,7 +1,6 @@
-import { LitElement, html, css, customElement, TemplateResult } from 'lit-element'
+import { LitElement, html, css } from 'lit-element'
 
-@customElement('vellum-stat-block-divider')
-export class StatBlockDivider extends LitElement {
+class StatBlockDivider extends LitElement {
 
   static get styles() {
     return css`
@@ -14,9 +13,13 @@ export class StatBlockDivider extends LitElement {
     }`
   }
 
-  render(): TemplateResult | void {
+  render() {
     return html`
     <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>`
   }
 
+  static get is() { return 'vellum-stat-block-divider' }
+
 }
+
+customElements.define(StatBlockDivider.is, StatBlockDivider)
