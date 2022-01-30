a56fc3f8f353af78e608dc6b8c1647410f2a1d18
R.G. Wood <ric@grislyeye.com>
2022-01-30 16:16:57 +0000

    Fix: dividers not appearing in html2canvas
    Due to an issue in html2canvas, the SVG dividers are not appearing when
    exporting to images.
    
    This commit removes (redundant) CSS styles that are the cause of this
    conflict:
    
    https://stackoverflow.com/a/59162619/48611

--- vellum-stat-block-divider.js
+++ vellum-stat-block-divider.js
@@ -4,18 +4,20 @@ class StatBlockDivider extends LitElement {
 
   static get styles() {
     return css`
+
     svg {
       fill: var(--stat-block-divider-color, lightgrey);
       /* Stroke is necessary for good antialiasing in Chrome. */
       stroke: var(--stat-block-divider-color, lightgrey);
-      margin-top: 0.6em;
-      margin-bottom: 0.35em;
     }`
   }
 
   render() {
     return html`
-    <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>`
+      <div class="container">
+        <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>
+      </div>
+    `
   }
 
   static get is() { return 'vellum-stat-block-divider' }
