1fbe105f11b90ac872b153d8ab67552dd7be395e
R.G. Wood <ric@grislyeye.com>
2022-01-31 11:41:40 +0000

    Fix: uneven horizontal divider spacing

--- vellum-stat-block-divider.js
+++ vellum-stat-block-divider.js
@@ -4,6 +4,10 @@ class StatBlockDivider extends LitElement {
 
   static get styles() {
     return css`
+    #container {
+      padding-top: 0.3em;
+      padding-bottom: 0.3em;
+    }
 
     svg {
       fill: var(--stat-block-divider-color, lightgrey);
@@ -14,7 +18,7 @@ class StatBlockDivider extends LitElement {
 
   render() {
     return html`
-      <div class="container">
+      <div id="container">
         <svg width="100%" height="100%" viewBox="0 0 400 5" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;" preserveAspectRatio="none"><path d="M0,0l400,2.5l-400,2.5"></path></svg>
       </div>
     `
